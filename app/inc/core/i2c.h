8afbac8c576282358f66e1a35d740f66860e7b3a
atomvn <hnvanhao@gmail.com>
2026-01-03 21:36:22 +0700

    config: untrack .o files

--- app/inc/core/i2c.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void i2c1_setup(void);
-void i2c_write(uint8_t addr, uint8_t reg, uint8_t data);
-void i2c_read(uint8_t addr, uint8_t reg, uint8_t *buf, uint8_t len);
